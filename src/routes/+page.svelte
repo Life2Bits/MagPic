d2a324a3899923071e291b44c833fc7ca366a2e2
Naoyuki Hirayama <naoyuki.hirayama@gmail.com>
2025-08-07 17:15:20 +0900

    Update landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h1>Welcome to MagPic</h1>
+<p>Powered by SvelteKit and deployed on Cloudflare Workers</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
